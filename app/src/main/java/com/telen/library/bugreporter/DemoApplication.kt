8c52550f600d265d7ab89862970f1ae6eda482ec
Karim Yarboua <karim.yarboua@gmail.com>
2021-04-18 02:55:53 +0200

    Bugreporter -- Removed useless import and v1.0.3

--- app/src/main/java/com/telen/library/bugreporter/DemoApplication.kt
+++ app/src/main/java/com/telen/library/bugreporter/DemoApplication.kt
@@ -3,7 +3,6 @@ package com.telen.library.bugreporter
 import android.app.Application
 import com.github.kaygenzo.bugreporter.BugReporter
 import com.github.kaygenzo.bugreporter.ReportMethod
-import com.github.kaygenzo.bugreporter.ReportResult
 
 class DemoApplication: Application() {
 
